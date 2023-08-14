8c7dfb480e405dbae022e58b7364ef6719264592
junkai.wang <openwjk@gmail.com>
2023-08-14 18:19:26 +0800

    提交login

---
+++ central-web/src/main/java/com/openwjk/central/web/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.openwjk.central.web.controller;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangjunkai
+ * @description
+ * @date 2023/8/14 18:18
+ */
+@RestController
+@RequestMapping("/login")
+@ApiOperation("登录控制层")
+@Log4j2
+public class LoginController {
+}
