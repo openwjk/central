c7f6f69f6af35e6c581be30232a08579f7320c95
junkai.wang <openwjk@gmail.com>
2023-08-29 13:42:05 +0800

    提交dubbo

--- central-service/src/main/java/com/openwjk/central/service/impl/DubboTestServiceImpl.java
+++ central-service/src/main/java/com/openwjk/central/service/impl/DubboTestServiceImpl.java
@@ -1,6 +1,7 @@
 package com.openwjk.central.service.impl;
 
 import com.openwjk.central.service.service.DubboTestService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
 /**
@@ -9,9 +10,11 @@ import org.apache.dubbo.config.annotation.DubboService;
  * @date 2023/8/27 19:06
  */
 @DubboService(interfaceClass = DubboTestService.class)
+@Slf4j
 public class DubboTestServiceImpl implements DubboTestService {
     @Override
     public String test() {
+        log.info("DubboTestServiceImpl.tset success ");
         return "success.";
     }
 }
