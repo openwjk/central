c7eba6c358b6234f18061fb79158a719b18a67a7
junkai.wang <openwjk@gmail.com>
2023-12-27 15:53:10 +0800

    提交节假日提醒

--- central-web/src/test/java/com/openwjk/central/SearchBaiduTest.java
+++ central-web/src/test/java/com/openwjk/central/SearchBaiduTest.java
@@ -1,12 +1,6 @@
-package com.openwjk.central.service.impl;
+package com.openwjk.central;
 
 import com.google.common.collect.Maps;
-import com.openwjk.central.commons.enums.QwRobotEnum;
-import com.openwjk.central.commons.enums.ScheduledTaskEnum;
-import com.openwjk.central.remote.dto.request.QwRobotReqDTO;
-import com.openwjk.central.remote.service.QwRobotService;
-import com.openwjk.central.service.domain.req.DdnsWebhookReqVO;
-import com.openwjk.central.service.service.ScheduledService;
 import com.openwjk.commons.utils.Constant;
 import com.openwjk.commons.utils.DateUtil;
 import com.ruiyun.jvppeteer.core.Puppeteer;
@@ -16,67 +10,41 @@ import com.ruiyun.jvppeteer.core.page.ElementHandle;
 import com.ruiyun.jvppeteer.core.page.Page;
 import com.ruiyun.jvppeteer.options.LaunchOptions;
 import com.ruiyun.jvppeteer.options.LaunchOptionsBuilder;
-import lombok.SneakyThrows;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author wangjunkai
  * @description
- * @date 2023/8/23 21:53
+ * @date 2023/12/27 11:03
  */
-@Service
-public class ScheduleFestivalReminderImpl implements ScheduledService {
+public class SearchBaiduTest {
     private static final Map<Integer, String> weekMap = Maps.newHashMap();
 
     static {
-        weekMap.put(1, "周日");
-        weekMap.put(2, "周一");
-        weekMap.put(3, "周二");
-        weekMap.put(4, "周三");
-        weekMap.put(5, "周四");
-        weekMap.put(6, "周五");
-        weekMap.put(7, "周六");
+        weekMap.put(1,"周日");
+        weekMap.put(2,"周一");
+        weekMap.put(3,"周二");
+        weekMap.put(4,"周三");
+        weekMap.put(5,"周四");
+        weekMap.put(6,"周五");
+        weekMap.put(7,"周六");
     }
 
-    @Autowired
-    @Qualifier("qwRobotService")
-    QwRobotService qwRobotService;
-
-    @Override
-    public ScheduledTaskEnum getCode() {
-        return ScheduledTaskEnum.FESTIVAL_OVERTIME;
-    }
-
-    @Override
-    public void execute(Date date) {
-        String verbalTrick = getBaiduData();
-        if (StringUtils.isNotBlank(verbalTrick)) {
-            sendRobotMsg(verbalTrick);
-        }
-    }
-
-    public void sendRobotMsg(String verbalTrick) {
-        QwRobotReqDTO reqDTO = new QwRobotReqDTO();
-        reqDTO.setRobotEnum(QwRobotEnum.XXW);
-        reqDTO.setVerbalTrick(verbalTrick);
-        qwRobotService.sendTextRobot(reqDTO);
-    }
-
-    @SneakyThrows
-    private String getBaiduData() {
+    @Test
+    void name() throws InterruptedException, IOException, ExecutionException {
         // 初始化浏览器相关
         BrowserFetcher.downloadIfNotExist(null);
         ArrayList<String> argList = new ArrayList<>();
@@ -101,30 +69,48 @@ public class ScheduleFestivalReminderImpl implements ScheduledService {
         Document document = Jsoup.parse(content);
         // 找出我们上面说的那个class所在的div标签
         Elements elements = document.getElementsByClass("item_uMLQg");
-        String verbalTrick=findTomorrowTag(elements);
-        browser.close();
-        return verbalTrick;
+//        // 去 class所在的div标签中找出需要的 字段信息
+//        for (int i = 0; i < elements.size(); i++) {
+//            Element element = elements.get(i);
+//            String date = element.getElementsByClass("WA_LOG_BTN").attr("data-module");
+//            System.out.println(date);
+//            if (!StringUtils.equals(DateUtil.formatDate(DateUtil.getNow(), "yyyy.MM.dd"), date)) {
+//                continue;
+//            }
+//            Elements tagEles =  element.getElementsByClass("tag_4m9Nx");
+//            if(CollectionUtils.isNotEmpty(tagEles)){
+//                System.out.println(tagEles.get(Constant.INT_ZERO).text());
+//            }
+//            System.out.println(1);
+//        }
+        System.out.println(findTomorrowTag(elements));
+    }
+
+    @Test
+    void name2() {
+        System.out.println(DateUtil.formatDate(DateUtil.plusDays(new Date(),5),"yyyy.M.d"));
     }
 
     private String findTomorrowTag(Elements elements) {
         for (int i = 0; i < elements.size(); i++) {
             Element element = elements.get(i);
             String baiduDate = element.getElementsByClass("WA_LOG_BTN").attr("data-module");
+            System.out.println(baiduDate);
             if (!StringUtils.equals("2024.2.4", baiduDate)) {
                 continue;
             }
-            String tomorrow = "2024-02-04";
-            int week = getDayOfWeek(tomorrow);
+            String tomorrow = DateUtil.formatDate(DateUtil.getTomorrowAtStart(), DateUtil.FORMAT_DATE_NORMAL);
+            int week = getDayOfWeek("2024-02-04");
             Elements tagEles = element.getElementsByClass("tag_4m9Nx");
             if (CollectionUtils.isNotEmpty(tagEles)) {
                 String tag = tagEles.get(Constant.INT_ZERO).text();
                 if (week == 1 || week == 7) {
                     if (StringUtils.equals("班", tag)) {
-                        return "明天是" + tomorrow + "，" + weekMap.get(week) + "，为补班日，记得调好闹钟哦~";
+                        return "明天是" + tomorrow + "," + weekMap.get(week) + ",为补班日，记得调好闹钟哦~";
                     }
                 } else {
                     if (StringUtils.equals("休", tag)) {
-                        return "明天是" + tomorrow + "，" + weekMap.get(week) + "，为假期休息日，记得关闭闹钟哦~";
+                        return "明天是" + tomorrow + "," + weekMap.get(week) + ",为假期休息日，记得关闭闹钟哦~";
                     }
                 }
             }
