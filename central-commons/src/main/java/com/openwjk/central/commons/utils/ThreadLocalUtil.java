d72bcd61015eb613a97c6e5e96f643e136517b43
openwjk <openwjk@gmail.com>
2024-09-04 18:18:50 +0800

    commit threadlocal

--- central-commons/src/main/java/com/openwjk/central/commons/utils/ThreadLocalUtil.java
+++ central-commons/src/main/java/com/openwjk/central/commons/utils/ThreadLocalUtil.java
@@ -1,11 +1,16 @@
 package com.openwjk.central.commons.utils;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
 import com.openwjk.central.commons.domain.LoginAccountDTO;
 import org.apache.logging.log4j.ThreadContext;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 
 public class ThreadLocalUtil {
-    private static final ThreadLocal<LoginAccountDTO> LOCAL_LOGIN_ACCOUNT_DTO = new ThreadLocal<>();
+    private static final TransmittableThreadLocal<LoginAccountDTO> LOCAL_LOGIN_ACCOUNT_DTO = new TransmittableThreadLocal<>();
+
+    private static final TransmittableThreadLocal<ConcurrentHashMap<String, String>> LOCAL_PARAMS = new TransmittableThreadLocal<>();
 
     public static void setLogId(String logId) {
         ThreadContext.put(Constants.LOCAL_REQ_LOG_ID, logId);
@@ -41,6 +46,26 @@ public class ThreadLocalUtil {
 
     public static void removeLoginDto() {
         LOCAL_LOGIN_ACCOUNT_DTO.remove();
+    }
+
+    public static void setLocalParams(String key, String value) {
+        LOCAL_PARAMS.get().put(key, value);
+    }
+
+    public static ConcurrentHashMap<String, String> getLocalParams() {
+        return LOCAL_PARAMS.get();
+    }
+
+    public static String getLocalParamsByKey(String key) {
+        return LOCAL_PARAMS.get().get(key);
+    }
+
+    public static void removeLocalParamsByKey(String key) {
+        LOCAL_PARAMS.get().remove(key);
+    }
+
+    public static void removeLocalParams() {
+        LOCAL_PARAMS.remove();
     }
 
 }
